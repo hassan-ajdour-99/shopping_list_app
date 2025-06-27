ab0dd62a1a5c6d17908f8bc2bee04cfd6f4ac22a
Mac <ajdourelhassa2096@gmail.com>
2025-06-27 13:11:44 +0100

    add all rep files

---
+++ lib/widget/grocery_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/dummy_data.dart';
+
+class GrocertList extends StatelessWidget {
+  const GrocertList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Your Groceries")),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder:
+            (ctx, index) => ListTile(
+              title: Text(groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: groceryItems[index].category.color,
+              ),
+              trailing: Text(groceryItems[index].quantity.toString()),
+            ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/new_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class NewItem extends StatelessWidget {
+  const NewItem({super.key});
+
+  @override
+  Widget build(BuildContext contex) {
+    return Text("");
+  }
+}
+
+class
