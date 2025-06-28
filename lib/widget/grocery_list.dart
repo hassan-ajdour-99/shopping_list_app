e20514f6c56a781085d590c07ce8c50fc745d9ce
Mac <ajdourelhassa2096@gmail.com>
2025-06-28 19:28:20 +0100

    add new chnages

--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/dummy_data.dart';
+import 'package:shopping_list_app/widget/new_item.dart';
 
-class GrocertList extends StatelessWidget {
+class GrocertList extends StatefulWidget {
   const GrocertList({super.key});
+
+  @override
+  State<GrocertList> createState() => _GrocertListState();
+}
+
+class _GrocertListState extends State<GrocertList> {
+  // Route to the NewItem Screen
+  void _addItem() {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Your Groceries")),
+      appBar: AppBar(
+        title: Text("Your Groceries"),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder:
--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -1,12 +1,36 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-class NewItem extends StatelessWidget {
+class NewItem extends StatefulWidget {
   const NewItem({super.key});
 
   @override
-  Widget build(BuildContext contex) {
-    return Text("");
+  State<NewItem> createState() {
+    return _NewItemState();
   }
 }
 
-class
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Add new Item")),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(label: Text("add a new item")),
+                validator: (value) {
+                  return "please enter a valid item";
+                },
+              ),
+              Row(children: []),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
